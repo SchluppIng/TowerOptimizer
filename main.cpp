879401be43be4ecf2dabb79453730106a7d8e78c
SchluppIng <currysde@web.de>
2022-01-29 09:38:17 +0100

    Added simple test data

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
     //Test Data
-    double maxDia {5.0}; //m
-    double minDia {2.0}; //m
+    double maxDia {6.0}; //m
+    double minDia {3.0}; //m
     double towerLength{80}; //m
     double maxSectionLength{40.0}; //m
     double maxSheetLength{2.95}; //m
