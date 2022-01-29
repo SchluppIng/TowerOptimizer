a434cf8dd1e6d2057c617fb77850d7534a04ecb6
SchluppIng <currysde@web.de>
2022-01-29 09:36:09 +0100

    First simple test data

--- main.cpp
+++ main.cpp
@@ -5,8 +5,19 @@ using namespace std;
 
 int main()
 {
-    double d{500};
-    InputData ipData(d);
+    //Test Data
+    double maxDia {5.0}; //m
+    double minDia {2.0}; //m
+    double towerLength{80}; //m
+    double maxSectionLength{40.0}; //m
+    double maxSheetLength{2.95}; //m
+    bool hasDoor{false};
+    double maxWeight{500}; //ton
+    double maxSectionWeight{250}; //ton
+    double topMass{500}; //ton
+
+    //Test Program
+    InputData ipData(maxWeight);
 
     ipData.ToString();
 
