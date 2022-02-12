d8d8bb9d91c7c34f43598d4f0e2b1ea61579144d
SchluppIng <currysde@web.de>
2022-02-12 20:52:33 +0100

    Added Tower Preliminary

--- Tower/Tower.h
+++ Tower/Tower.h
@@ -1,10 +1,36 @@
 #ifndef TOWER_H
 #define TOWER_H
 
+#include "TowerSection.h"
+
 class Tower
 {
 public:
+    Tower(const std::vector<TowerSection>& vecTowerSections, const double& headMass);
+    Tower(const Tower& tower);
+
+    ~Tower(){}
+
+    double getHeadMass() const { return topMass;}
+    void setHeadMass(const double& headMass) { topMass = headMass;}
+
+    double getTowerBottomDiameter() const { return bottomDiameter;}
+    double getTowerTopDiameter() const { return topDiameter; }
+    int getNumberOfSections() const { return numberOfSections; }
+    double getTowerWeight() const { return towerWeight; }
+
 private:
+
+    void calcTowerWeight();
+
+    std::vector<TowerSection> vecTowerSection;
+    double topMass;
+    double towerTilt;
+
+    double bottomDiameter;
+    double topDiameter;
+    int numberOfSections;
+    double towerWeight;
 };
 
 #endif
