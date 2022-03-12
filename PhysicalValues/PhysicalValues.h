83154f1e04220e0482bca6cec361825db68ec87c
SchluppIng <currysde@web.de>
2022-03-12 08:33:10 +0100

    Added gravity, EModul

--- PhysicalValues/PhysicalValues.h
+++ PhysicalValues/PhysicalValues.h
@@ -2,7 +2,9 @@
 #define PHYSICAL_VALUES_H
 
 //Global variables;
-const double steelWeigth{7.85};
+const double steelWeigth{7.85}; //t/m³
+const int youngsModul{210000}; // Youngs modul steel in N/mm²
+const double gravity{9.81}; //m/s²
 
 class PhysicalValues
 {
